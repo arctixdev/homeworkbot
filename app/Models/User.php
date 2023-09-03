2e404766215e01161381ed0426339f04c133b5c9
Jonathan Bangert <jonathan@bangert.dk>
2023-09-03 12:13:06 +0200

    Initial testing

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'username',
     ];
 
     /**
@@ -29,8 +27,6 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
-        'password',
-        'remember_token',
     ];
 
     /**
@@ -39,7 +35,5 @@ class User extends Authenticatable
      * @var array<string, string>
      */
     protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
